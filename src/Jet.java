8e1e0234d3ac131aee2e340ac6b38f7faacf1ac4
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-04-30 01:33:28 +0100

    Updates

--- src/Jet.java
+++ src/Jet.java
@@ -1,3 +1,17 @@
-public class Jet {
+public class Jet implements FlightEnabled {
 
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
 }
