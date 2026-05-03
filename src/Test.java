9417529e75c0fc355c0f480eef53931b71eddc32
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-05-03 20:57:43 +0100

    Updates

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,19 @@
+
 public class Test {
 
     public static void main(String[] args) {
 
+        inFlight(new Jet());
     }
 
-    private  static void inFlight(FlightEnabled flier) {
+    private static void inFlight(FlightEnabled flier) {
 
         flier.takeOff();
-        flier.
+        flier.transition(FlightStages.LAUNCH);
+        flier.fly();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
+        flier.land();
     }
 }
