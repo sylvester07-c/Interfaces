01eb8b19a0408948485e64925e2b5c031b480473
Sylvester Praise <sylvesterpraise123456@gmail.com>
2026-05-03 20:55:11 +0100

    Updates

--- src/Bird.java
+++ src/Bird.java
@@ -1,32 +1,29 @@
-public class Bird extends Animal implements  FlightEnabled, Trackable {
+
+
+public class Bird extends Animal implements FlightEnabled, Trackable {
 
     @Override
     public void move() {
-        System.out.println("Flap Wings");
+        System.out.println("Flaps wings");
     }
-
 
     @Override
     public void takeOff() {
-        System.out.println(getClass().getSimpleName() + " is taking off ");
+        System.out.println(getClass().getSimpleName() + " is taking off");
     }
 
     @Override
     public void land() {
-        System.out.println(getClass().getSimpleName() + " is landing ");
-
+        System.out.println(getClass().getSimpleName() + " is landing");
     }
 
     @Override
     public void fly() {
-
         System.out.println(getClass().getSimpleName() + " is flying");
-
     }
 
     @Override
     public void track() {
-        System.out.println(getClass().getSimpleName() + " 's coordinates recored");
-
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
     }
 }
